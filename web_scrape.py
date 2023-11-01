d4f10406942df52a910b7123a65faa98307897f3
Zach1031 <zachdef31@gmail.com>
2023-11-01 18:05:26 -0400

    Added title web scraping

--- web_scrape.py
+++ web_scrape.py
@@ -7,10 +7,12 @@ with open("aita.html") as fp:
 
 tag_visible = ['p', 'li']
 
+title = soup.find_all('shreddit-title')[0]['title'].split(":")[0].strip()
+
 post = soup.find_all('div', id=lambda x: x and 'post' in x)[0].find_all('p')
 
 pars = []
 for par in post:
     pars.append(par.text)
 
-print('\n'.join(pars))
+fullPost = '\n'.join(pars)
