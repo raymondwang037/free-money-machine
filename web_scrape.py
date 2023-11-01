850b70468ccda0d07b24506cad93e0135d2253df
Zach1031 <zachdef31@gmail.com>
2023-11-01 17:53:45 -0400

    Basic webscraping + testing files

--- web_scrape.py
+++ web_scrape.py
@@ -2,3 +2,15 @@ import requests
 
 from bs4 import BeautifulSoup
 
+with open("aita.html") as fp:
+    soup = BeautifulSoup(fp, 'html.parser')
+
+tag_visible = ['p', 'li']
+
+post = soup.find_all('div', id=lambda x: x and 'post' in x)[0].find_all('p')
+
+pars = []
+for par in post:
+    pars.append(par.text)
+
+print('\n'.join(pars))
