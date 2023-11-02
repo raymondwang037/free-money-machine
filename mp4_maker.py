dff032875e1831cb7080a41cec015fb153028027
William Kwon <43156054+WilmKwn@users.noreply.github.com>
2023-11-01 23:39:41 -0400

    video editing part - skeleton

--- mp4_maker.py
+++ mp4_maker.py
@@ -2,7 +2,7 @@ from moviepy import *
 
 from mp3_maker import mp3_files
 
-for i in range(0, len(mp3_files)):
+for i in range(0, len(mp3_files)-1):
     video = VideoFileClip("myHolidays.mp4").subclip(i*60,(i+1)*60)
     audio = AudioFileClip(mp3_files[i])
     video.set_audio(audio)
