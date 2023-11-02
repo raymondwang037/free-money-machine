3e84cb6afe995d849729f307c1236eada0b1be5c
Frank Casella <frankcasella17@gmail.com>
2023-11-02 16:52:01 -0400

    Planning for how to prepare the text the last time before putting into the voice generator

---
+++ text_prepper.py
@@ -0,0 +1,10 @@
+import re
+
+# read the files
+# remove the separators
+# every sentence is its own list element
+# if two consecutive sentences are < 200 characters join
+# if sentence is greater than 200 characters count commas
+# Should be the start of an array element, going until the first comma
+# if two commas, have three separate array elements 100%%%%
+# do we export to a new text file which each array element as a new line?
