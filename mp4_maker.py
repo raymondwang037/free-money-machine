f9ae235dc5e67b99bf1b581cb0515052c44636c7
William Kwon <43156054+WilmKwn@users.noreply.github.com>
2023-11-02 15:57:06 -0400

    Concatenates audios, combine it with minecraft video, and export as mp4

--- mp4_maker.py
+++ mp4_maker.py
@@ -1,13 +1,26 @@
-from moviepy import *
+from moviepy.editor import *
 
 from mp3_maker import mp3_files
 
-for i in range(0, len(mp3_files)-1):
-    video = VideoFileClip("myHolidays.mp4").subclip(i*60,(i+1)*60)
-    audio = AudioFileClip(mp3_files[i])
-    video.set_audio(audio)
+count = 0
+index = 0
+while index < len(mp3_files):
+    video = VideoFileClip("minecraft.mp4").subclip(count*60,(count+1)*60)
+    audios = []
+    duration = 0
+    for i in range(index, len(mp3_files)):
+        curr = AudioFileClip(mp3_files[i])
+        if duration + curr.duration > 60:
+            break
+        duration += curr.duration
+        audios.append(curr)
+        index += 1
+
+    concat_audio = concatenate_audioclips(audios)
+    video.set_audio(concat_audio)
 
     # TODO: add the subtitle thing
 
-    filename = 'p' + i + '.mp4'
+    filename = 'p' + count + '.mp4'
     video.write_videofile(filename)
+    count += 1
