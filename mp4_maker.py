27b528065f9bba96901741862d2d08e909701bd3
William Kwon <43156054+WilmKwn@users.noreply.github.com>
2023-11-01 23:36:59 -0400

    video editing part - skeleton

--- mp4_maker.py
+++ mp4_maker.py
@@ -4,9 +4,10 @@ from mp3_maker import mp3_files
 
 for i in range(0, len(mp3_files)):
     video = VideoFileClip("myHolidays.mp4").subclip(i*60,(i+1)*60)
+    audio = AudioFileClip(mp3_files[i]).set_start(0)
 
     # TODO: add the subtitle thing
 
-    result = CompositeVideoClip([video])
+    result = CompositeVideoClip([video, audio])
     filename = 'p' + i + '.mp4'
     result.write_videofile(filename, fps=25)
