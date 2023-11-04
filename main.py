0fc0a38abd8ddc7e3e490f42fd89b362024798d0
Raymond Wang <raymondwang037@gmail.com>
2023-11-04 16:50:01 -0400

    small change

--- main.py
+++ main.py
@@ -18,5 +18,12 @@ def main():
         subreddit.export()
     # pass
 
+    posts_path = os.getcwd() + "/posts"
+    posts_list = os.listdir(posts_path)
+
+    c = 0
+    for e in posts_list:
+        os.system("python3 mp3_maker.py -v en_us_001 -f " +  posts_path + "/" + e + " -n " + e + ".mp3")
+
 if __name__ == "__main__":
     main()
