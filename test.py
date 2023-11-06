af4a2ed5fea135d749b1406ccc5c34e721fac95a
Zach1031 <zachdef31@gmail.com>
2023-11-06 15:52:35 -0500

    TTS from text file works

--- test.py
+++ test.py
@@ -1,20 +1,33 @@
-import requests, base64, random, argparse, os, playsound, time, re, textwrap, json
+import requests, base64, random, argparse, os, playsound, time, re, textwrap, json, time
 
 
 ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
 
 headers = {'Content-type': 'application/json'}
 
+f = open("./posts/I messed up the tire on my parents van while running errands", "r")
+
+inc = 0
+for line in f.readlines():
+    time.sleep(1.5)
+    line = line.strip()
+    rest_array = [text.encode("utf8") for text in line]
+    line = b''.join(rest_array)
+    url = "{endpoint}/api/generation".format(endpoint=ENDPOINT)
+    json = b'{"text": "' + line + b'", "voice": "en_us_001"}'
+    r = requests.post(url, json, headers=headers).json()
+
+    vstr = r["data"]
+
+    b64d = base64.b64decode(vstr)
+
+    os.system("touch {num}.mp3".format(num=inc))
+
+    out = open("{num}.mp3".format(num=inc), "wb")
+    out.write(b64d)
+    out.close()
+
+    # playsound.playsound("{num}.mp3".format(num=inc))
 
 
-r = requests.post(ENDPOINT + "/api/generation", '{"text": "it works now", "voice": "en_us_ghostface"}', headers=headers).json()
 
-vstr = r["data"]
-
-b64d = base64.b64decode(vstr)
-
-out = open("test.mp3", "wb")
-out.write(b64d)
-out.close()
-
-playsound.playsound("test.mp3")
